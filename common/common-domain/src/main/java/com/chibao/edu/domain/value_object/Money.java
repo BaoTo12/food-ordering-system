3f3e88ec74118976b9a1e8039d031953f12315ac
ChiBao <baotochi87@gmail.com>
2025-09-29 07:39:01 +0700

    feat(order): add common value objects

---
+++ common/common-domain/src/main/java/com/chibao/edu/domain/value_object/Money.java
@@ -0,0 +1,52 @@
+package com.chibao.edu.domain.value_object;
+
+import lombok.Getter;
+
+import java.awt.event.MouseEvent;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+@Getter
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public boolean isGreaterThanZero(){
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money){
+        return this.amount !=  null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money){
+        return new Money(this.amount.add(money.getAmount()));
+    }
+
+    public Money subtract(Money money){
+        return new Money(this.amount.subtract(money.getAmount()));
+    }
+    public Money multiply(int multiplier){
+        return new Money(this.amount.multiply(new BigDecimal(multiplier)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
+    }
+
+    private BigDecimal setScale(BigDecimal input){
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}

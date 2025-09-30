214b6c58f2ee6d6e6f7bd7182b4d3ea690a0b406
ChiBao <baotochi87@gmail.com>
2025-09-30 10:44:31 +0700

    chore: remove some uneccesary comments

--- order-service/order-domain/order-application-service/src/main/java/com/chibao/edu/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/chibao/edu/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
@@ -3,6 +3,7 @@ package com.chibao.edu.domain.ports.output.message.publisher.payment;
 import com.chibao.edu.domain.event.OrderCancelledEvent;
 import com.chibao.edu.domain.event.publisher.DomainEventPublisher;
 
+// ? An interface that defines how to publish a message when an order is created.
 public interface OrderCancelledPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCancelledEvent> {
 
 }
